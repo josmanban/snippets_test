e37c3bbbbe48ea3afcd968819a9a1c2905c8d610
josmanban <josemanuelbanda@gmail.com>
2025-02-03 17:32:08 +0000

    [main] update some celery options

--- django_snippets/celery.py
+++ django_snippets/celery.py
@@ -1,11 +1,18 @@
-import os
+import os, ssl
 
 from celery import Celery
 from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_snippets.settings")
 
-app = Celery("django_snippets")
+app = Celery("django_snippets",
+             broker_use_ssl = {
+        'ssl_cert_reqs': ssl.CERT_NONE
+     },
+     redis_backend_use_ssl = {
+        'ssl_cert_reqs': ssl.CERT_NONE
+     }
+             )
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
